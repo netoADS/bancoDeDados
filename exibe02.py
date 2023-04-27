a699818c8e1eef526da959d59cb7e84c84b888db
EC2 Default User <ec2-user@ip-172-31-11-38.ec2.internal>
2023-04-27 00:57:16 +0000

    260420232157

---
+++ exibe02.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+# Conectando ao banco de dados
+config = {
+  'user': 'admin',
+  'password': 'aulanoiteFaculdade',
+  'host': 'dbaula.clstmfvvjfyt.us-east-1.rds.amazonaws.com',
+  'database': 'aula'
+}
+
+# Estabelecer a conexão com o banco de dados
+try:
+    conn = mysql.connector.connect(**config)
+    print("Conexão executada com sucesso.")
+except mysql.connector.Error as err:
+    print(f"Conexão falhou: {err}")
+
+# Criando um objeto cursor para executar as consultas SQL
+cursor = conn.cursor()
+
+#inserindo o estado na tabela
+sql = "SELECT * FROM estado"
+cursor.execute(sql)
+
+#obter o resultado da consulta
+result = cursor.fetchall()
+print(result)
